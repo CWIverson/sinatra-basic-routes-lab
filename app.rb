1059871f8def9e4a8c03a21dd23b4761eb2182f8
christian iverson <chris.w.iverson@gmail.com>
2021-02-12 12:42:08 -0800

    commit tree 6a78f94b00dddf3367b2399c795c537e5d4d8396

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Christian"
+    end
+
+    get '/hometown' do
+        "My hometown is Sedro-Woolley"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sing about me/I'm dying of thirst"
+    end
+
 end
